2a44b97a493a5c1d4bdc42e7ee14b850e4e8edee
Joanna Szpadkiewicz <joanna.szpadkiewicz@gmail.com>
2020-07-23 21:28:01 +0200

    dodana lista obiektow do api.js

--- src/server/api.js
+++ src/server/api.js
@@ -5,7 +5,7 @@ router.get("/", (req, res) => {
   res.send("Tu niech powstanie dokumentacja usług restowych");
 });
 
-router.post("/getMentors", (req, res) => {
+router.post("/getContractor", (req, res) => {
   console.log("req.body.params", req.body);
   let response = {
     data: {
@@ -13,16 +13,61 @@ router.post("/getMentors", (req, res) => {
         result: {
           items: [
             {
-              login: "lcichorek",
-              fullName: "TEst Testowy",
-              skype: "lukaszci06",
+              name: 'Drogi Pamiętniku',
+              nip: '106-34-00-062',
+              email: 'pamietnik@for.ever',
+              address: {
+                        street: 'Bądkowskiego',
+                        houseNumber:  '31',
+                        postalCode: '97-451',
+                        city: 'Kowale'
+                        },
             },
             {
-              login: "pszyszkowski",
-              fullName: "Paweł Szyszkowski",
-              skype: "szyszkowskipawell",
+              name: 'Pamiątki z podróży',
+              nip: '356-00-10-056',
+              email: 'pamiatkiz@gmail.com',
+              address: 	{
+                          street: 'Żuławskiego',
+                          houseNumber:  '4/6',
+                          postalCode: '52-501',
+                          city: 'Sztum'
+                        },
             },
-          ],
+            {
+              name: 'Domestos i spółka',
+              nip: '254-06-90-076',
+              email: 'biuro@chemiadopracy.com',
+              address: {
+                        street: 'Dolna',
+                        houseNumber:  '55',
+                        postalCode: '00-781',
+                        city: 'Warszawa'
+                      },
+            },
+            {
+              name: 'Dezynfex',
+              nip: '123-08-70-567',
+              email: 'biuro@dezynfex.com',
+              address: {
+                        street: 'Książęca',
+                        houseNumber:  '33',
+                        postalCode: '12-654',
+                        city: 'Grodzisk Mazowiecki'
+                        },
+          },
+          {
+              name: 'Kuchnia Węgierska',
+              nip: '134-12-10-042',
+              email: 'office@smakiwegier.com',
+              address: {
+                        street: '1000lecia',
+                        houseNumber:  '11',
+                        postalCode: '78-456',
+                        city: 'Jankowo'
+                        },
+          }
+      ],
         },
       },
     },
@@ -31,53 +76,7 @@ router.post("/getMentors", (req, res) => {
     res.status("200").json(response);
     res.end();
   }, 2000);
-});
 
-router.post("/getImportantLinks", (req, res) => {
-  console.log("req.body.params", req.body);
-  let response = {
-    data: {
-      data: {
-        result: {
-          items: [
-            {
-              text: "Dokumentacja IBM BPM",
-              href:
-                "https://www.ibm.com/support/knowledgecenter/en/SSFPJS_8.5.5/com.ibm.wbpm.main.doc/kc-homepage-bpm.html",
-            },
-            {
-              text: "NodeJS",
-              href: "https://nodejs.org/en/",
-            },
-            {
-              text: "Vue.js",
-              href: "https://vuejs.org/",
-            },
-            {
-              text: "Vue CLI",
-              href: "https://cli.vuejs.org/",
-            },
-            {
-              text: "Vuetify",
-              href: "https://vuetifyjs.com/en/",
-            },
-            {
-              text: "Github IBPM",
-              href: "https://github.com/IBPM-S-A",
-            },
-            {
-              text: "Wiki IBPM",
-              href: "https://wiki.ibpm.pro/",
-            },
-          ],
-        },
-      },
-    },
-  };
-  setTimeout(() => {
-    res.status("200").json(response);
-    res.end();
-  }, 2000);
 });
 
 module.exports = router;
