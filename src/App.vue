a319c8d9cbbf8e115c732b7cb63f4a62cd889ceb
Angela <angelikatyburska@gmail.com>
2020-07-30 20:48:01 +0200

    Modified app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,8 @@
 <template>
   <v-app>
-<<<<<<< HEAD
-    <Document :documentObject="document" />
-=======
     <v-content>
       <Document :documentObject="document" />
     </v-content>
->>>>>>> AT-117/136
   </v-app>
 </template>
 
@@ -21,36 +17,10 @@ export default {
 
   data() {
     return {
-<<<<<<< HEAD
-      // document: {
-      //   receiveDate: null,
-      //   issueDate: null,
-      //   paymentDate: null,
-      //   invoiceNumber: null,
-      //   expenseCategory: null,
-      //   invoiceComments: null,
-      //   // sumNetto: null,
-      //   // sumVat: null,
-      //   // sumBrutto: null,
-      //   documentContent: {
-      //     items: [],
-      //   },
-      // },
-      path: "/Akademia 2020/Testowy/Folder",
-      documents: {
-        items: [],
-      },
-      objectTypeId: "cmis:document",
-=======
->>>>>>> AT-117/136
       context: window.context,
       document: window.context ? window.context.binding.get("value") : {},
     };
   },
-<<<<<<< HEAD
-=======
-
->>>>>>> AT-117/136
   closeForm(document) {
     if (this.context) {
       this.context.binding.set("value", document);
